d16420f7f7f3b6b9aba869b25e80eb86f712a655
deftonechris <deftonechris@msn.com>
2023-10-25 16:25:37 -0600

    added more detail to links, instruction to mobile porfolio screen

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import applications from '../data/ProjectData';
-import { FaGithub } from 'react-icons/fa';
-import { HiOutlineLink } from 'react-icons/hi';
+import React, { useState } from "react";
+import applications from "../data/ProjectData";
+import { FaGithub } from "react-icons/fa";
+import { HiOutlineLink } from "react-icons/hi";
 
 const Work = () => {
   const [selectedProject, setSelectedProject] = useState(null);
@@ -10,7 +10,7 @@ const Work = () => {
     if (event) {
       event.stopPropagation();
     }
-  
+
     if (window.innerWidth < 768) {
       if (selectedProject === key) {
         setSelectedProject(null);
@@ -28,23 +28,49 @@ const Work = () => {
             Portfolio
           </p>
         </div>
+        <h4 className="text-center text-sm md:hidden">(Tap for more info)</h4>
         <div className="grid mx-auto grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           {applications.map((app) => (
-            <div key={app.key} className="project-card" onClick={() => toggleProject(app.key)}>
+            <div
+              key={app.key}
+              className="project-card"
+              onClick={() => toggleProject(app.key)}
+            >
               <div className="text-center">
-              <img className="project-image h-48 sm:h-80 md:h-64 lg:h-96" src={app.src} alt={app.name} />
+                <img
+                  className="project-image h-48 sm:h-80 md:h-64 lg:h-96"
+                  src={app.src}
+                  alt={app.name}
+                />
               </div>
-              <div className={`overlay ${selectedProject === app.key ? 'show' : ''}`}>
+              <div
+                className={`overlay ${
+                  selectedProject === app.key ? "show" : ""
+                }`}
+              >
                 <h2 className="font-bold text-center mb-4">{app.name}</h2>
                 <div className="text-center mb-4 md:mb-0">
-                  <div className="flex justify-center space-x-4">
-                    <a href={app.code} className="info mb-4">
-                      <FaGithub className="text-2xl hover:text-[#6cc644]" />
-                    </a>
-                    <a className="info" href={app.link}>
-                      <HiOutlineLink className="text-2xl hover:text-[#dE73FF]" />
-                    </a>
-                  </div>
+                <div className="flex justify-center space-x-4">
+  <div className="text-center">
+    <a
+      href={app.code}
+      className="info mb-4 hover:text-[#6cc644] flex flex-col items-center"
+    >
+      <FaGithub className="text-2xl" />
+      <span className="block text-xs mt-1 -ml-0.5">GitHub</span>
+    </a>
+  </div>
+  <div className="text-center">
+    <a
+      className="info hover:text-[#dE73FF] flex flex-col items-center"
+      href={app.link}
+    >
+      <HiOutlineLink className="text-2xl" />
+      <span className="block text-xs mt-1 -ml-0.5">Link</span>
+    </a>
+  </div>
+</div>
+
                 </div>
                 <div className="text-center">
                   <div className="tech-container">
@@ -68,8 +94,3 @@ const Work = () => {
 };
 
 export default Work;
-
-
-
-
-
